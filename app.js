c62d69b978d601007f8663df7eea56e01ac92ba9
Ronit <70330163+asawaronit60@users.noreply.github.com>
2021-06-24 11:04:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const mongoose  = require('mongoose')
 const express = require('express');
 const bodyParser = require('body-parser')
 const ejs = require('ejs');
-const { response } = require('express');
 const app = express();
 
 app.set('view-engine',ejs);
 app.use(bodyParser.urlencoded({
     extended:true
 }))
+
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
